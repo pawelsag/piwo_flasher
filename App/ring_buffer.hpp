08e1c885fe609562e3b6fe25c665aa9d8b5b88e7
WindSpring <psagan@antmicro.com>
2022-04-12 22:06:49 +0200

    adding semaphore to protect the rx_queue

--- App/ring_buffer.hpp
+++ App/ring_buffer.hpp
@@ -1,4 +1,8 @@
 #pragma once
+extern "C"
+{
+#include "pico/sem.h"
+}
 #include <array>
 #include <cmath>
 
@@ -12,28 +16,34 @@ struct ring_buffer{
 
   bool push_no_wait(T item)
   {
+    sem_acquire_blocking(&sem);
     int head_next;
     head_next = head;
 
     MODULO_INC(head_next, S);
     if (head_next == tail) {
+      sem_release(&sem);
       return false;
     }
 
     buf[head] = item;
     head = head_next;
+    sem_release(&sem);
     return true;
   }
 
   bool pop(T *item)
   {
+    sem_acquire_blocking(&sem);
     if(empty())
     {
+      sem_release(&sem);
       return false;
     }
 
     *item = buf[tail];
     MODULO_INC(tail, S);
+    sem_release(&sem);
     return true;
   }
 
@@ -45,16 +55,20 @@ struct ring_buffer{
   void
   reset()
   {
+    sem_acquire_blocking(&sem);
     head = tail = 0;
+    sem_release(&sem);
   }
 
   ring_buffer()
   {
+    sem_init(&sem, 1, 1);
   }
 
 private:
   int head=0, tail=0;
   std::array<T,S> buf;
+  semaphore_t sem;
 };
 
 inline ring_buffer<100, uint8_t> rx_queue;
